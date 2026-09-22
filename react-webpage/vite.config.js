e8822f4bf4340fa494981f4b349e03c6d8f9fa54
tommytongtty-coder <tommytong.tty@gmail.com>
2026-09-22 08:51:06 +0800

    Add allowed hosts for preview configuration

--- react-webpage/vite.config.js
+++ react-webpage/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['oopas.onrender.com'],
+    'oopas.onrender.com',
+    'institutionforgiftedchildren.org',
+    'www.institutionforgiftedchildren.org',
   },
 })
